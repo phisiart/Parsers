23216155625a6a1d1725006dc1f727e63c6dbc5e
Johnson Tan <phisiart@gmail.com>
2016-03-29 20:37:21 +0800

    forgot to add Tokens.cs

---
+++ Tokens/Tokens.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public abstract class Token { }
+
+public sealed class IntToken : Token {
+    public IntToken(Int32 val) {
+        this.Val = val;
+    }
+
+    public Int32 Val { get; }
+}
+
+public abstract class OpToken : Token { }
+
+public sealed class MultToken : OpToken { }
+
+public sealed class DivToken : OpToken { }
+
+public sealed class AddToken : OpToken { }
+
+public sealed class SubToken : OpToken { }
+
+public sealed class LeftParenToken : OpToken { }
+
+public sealed class RightParenToken : OpToken { }
